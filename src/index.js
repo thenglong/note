e9fa3d9963fad94ba230a66cf84c8cf0c56a126a
heng thenglong <49927604+thenglong@users.noreply.github.com>
2021-02-14 21:43:25 +0700

    ssUpdate index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
 <<<<<<< HEAD
+<<<<<<< HEAD
     <App prop="prop" />
 =======
     <App />
 >>>>>>> 9e9e66d ([v1] basic features)
+=======
+    <App prop="prop"/>
+>>>>>>> c5d2a19 (Update index.js)
   </React.StrictMode>,
   document.getElementById('root')
 );
